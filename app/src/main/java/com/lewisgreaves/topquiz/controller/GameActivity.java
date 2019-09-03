810be2188894727c39bdbe32441ef53e86f5b326
Konstantin <lewiswilliamgreaves@gmail.com>
2019-09-03 21:11:47 +0100

    Fixed the return message (put code inside onActivityResult instead of onCreate). Delayed the showing of the next question.

--- app/src/main/java/com/lewisgreaves/topquiz/controller/GameActivity.java
+++ app/src/main/java/com/lewisgreaves/topquiz/controller/GameActivity.java
@@ -7,6 +7,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -103,11 +104,17 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             Toast.makeText(GameActivity.this, "That's not right!", Toast.LENGTH_SHORT).show();
         }
-        if (--mNumberOfQuestions == 0) {
-            endGame();
-        } else {
-            mCurrentQuestion = mQuestionBank.getQuestion();
-            displayQuestion(mCurrentQuestion);
-        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (--mNumberOfQuestions == 0) {
+                    endGame();
+                } else {
+                    mCurrentQuestion = mQuestionBank.getQuestion();
+                    displayQuestion(mCurrentQuestion);
+                }
+            }
+        }, 2000);
     }
 }
--- app/src/main/java/com/lewisgreaves/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/lewisgreaves/topquiz/controller/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Selection;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
@@ -25,12 +26,11 @@ public class MainActivity extends AppCompatActivity {
     public static final String PREFERENCE_KEY_NAME = "PREFERENCE_KEY_NAME";
     public static final String PREFERENCE_KEY_SCORE = "PREFERENCE_KEY_SCORE";
 
-    private TextView mGreetingText = findViewById(R.id.activity_main_greeting_text);
+    private TextView mGreetingText;
     private EditText mNameInput;
     private Button mPlayButton;
     private User mUser;
     private SharedPreferences mPreferences;
-    private static String mReturnText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,19 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
         mNameInput = findViewById(R.id.activity_main_name_input);
         mPlayButton = findViewById(R.id.activity_main_play_btn);
+        mGreetingText = findViewById(R.id.activity_main_greeting_text);
         mUser = new User();
         mPreferences = getPreferences(0);
 
         mPlayButton.setEnabled(false);
-
-        String firstname = getPreferences(0).getString(PREFERENCE_KEY_NAME, null);
-        int score = getPreferences(0).getInt(PREFERENCE_KEY_SCORE, 0);
-        mReturnText = "Welcome back " + firstname + "! Your last score was " + score + ", will you do better this time?";
-
-        if (mPreferences.contains(firstname)) {
-            mGreetingText.setText(mReturnText);
-            mNameInput.setText(firstname);
-        } else {
 
             mNameInput.addTextChangedListener(new TextWatcher() {
                 @Override
@@ -81,15 +73,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GAME_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
-            assert data != null;
-            int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
-            mPreferences.edit().putInt(PREFERENCE_KEY_SCORE, score).apply();
+            if (data != null) {
+                int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
+                mPreferences.edit().putInt(PREFERENCE_KEY_SCORE, score).apply();
+            }
         }
+        String userName = getPreferences(0).getString(PREFERENCE_KEY_NAME, "Anonymous");
+        int score = getPreferences(0).getInt(PREFERENCE_KEY_SCORE, 0);
+        String returnText = "Welcome back " + userName + "! Your last score was " + score + ", will you do better this time?";
+        mGreetingText.setText(returnText);
+        mNameInput.setText(userName);
     }
 }
